6c11abcebf6f7ac14deb0fb4ce5310f0f2e8eb3c
Lalu Prasad <lalu.ec22@sahyadri.edu.in>
2026-02-17 23:16:40 +0530

    task

--- NaukriCom.java
+++
@@ -1,9 +0,0 @@
-class NaukriCom{
-public static void main(String []job){
-int experience = 2;
-int salary = 35000;
-String resume = "uploaded";
-System.out.println("User experience is "+ experience+" years");
-System.out.println("User salary is "+ salary+"thousand rupees");
-System.out.println("User resume is "+resume);
-}}
--- Twitter.java
+++
@@ -1,8 +0,0 @@
-class Twitter{
-public static void main(String [] X){
-long mobileNumber = 8217279387l;
-String emailAddress = "laluprasad2504@gmail.com";
-System.out.println("User Mobile Number is "+mobileNumber);
-System.out.println("User Email Address is "+emailAddress);
-
-}}
