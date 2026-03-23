d80771f162b8d967af82a6107ab6c1a5e1139e89
Lalu Prasad <lalu.ec22@sahyadri.edu.in>
2026-03-23 22:30:04 +0530

    is a relation ship

--- AirportRunner.java
+++
@@ -1,8 +0,0 @@
-class AirportRunner{
-public static void main(String []args){
-Terminal terminal = new Terminal(2,"Devanahalli");
-Airport airport = new Airport(1 , "Kempegowda International Airport" , "Devanahalli", terminal);
-airport.getAirportDetails();
-
-}
-}
